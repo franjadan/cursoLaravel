9ab482ee8887c53c2319a8a4b234017ba46b5da7
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2020-01-21 16:44:28 +0100

    Creadas rutas en listado y detalle de usuarios

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -8,4 +8,11 @@
 
     <p>Nombre del usuario: {{ $user->name }}</p>
     <p>Correo electrónico: {{ $user->email }}</p>
+
+    <p>
+        <!--<a href="{{ url('/usuarios') }}">Regresar</a>-->
+        <!--<a href="{{ url()->previous() }}">Regresar</a>-->
+        <!--<a href="{{ action('UserController@index') }}">Regresar</a>-->
+        <a href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
+    </p>
 @endsection
