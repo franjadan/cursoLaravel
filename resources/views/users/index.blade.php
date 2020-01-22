648f10c8569622c4afe28fe2280271bb0a6c4df3
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2020-01-22 15:59:15 +0100

    Mejorado listado de usuarios

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,19 +5,39 @@
 @section('content')
 
     <h1>{{ $title }}</h1>
-    
-    <ul>
-        @forelse ($users as $user)
-            <li>
-                {{ $user->name }}, ({{ $user->email }})
-                <!--<a href="{{ url("/usuarios/{$user->id}") }}">Ver detalles</a>-->
-                <!--<a href="{{ action('UserController@show', ['id' => $user->id]) }}">Ver detalles</a>-->
-                <a href="{{ route('users.show', ['id' => $user->id]) }}">Ver detalles</a>
-            </li>
-        @empty
-        <li>No hay usuarios registrados</li>
-        @endforelse
-    </ul>
+
+    @if(!$users->isEmpty())
+        <table class="table table-bordered table-striped">
+            <thead>
+            <tr>
+                <th scope="col">Nombre</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Profesión</th>
+                <th scope="col">Admin</th>
+            </tr>
+            </thead>
+            <tbody>
+                @foreach ($users as $user)
+                <tr>
+                    <td><a href="{{ route('users.show', ['id' => $user->id]) }}">{{ $user->name }}</a></td>
+                    <td>{{ $user->email }}</td>
+                    @if(isset($user->profession))
+                        <td>{{ $user->profession->title }}</td>
+                    @else
+                        <td></td>
+                    @endif
+                    @if ($user->is_admin == 1)
+                        <td><i class="fas fa-check text-success"></i></td>
+                    @else
+                        <td><i class="fas fa-times text-danger"></i></td>
+                    @endif
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @else
+        <p>No hay usuarios registrados</p>
+    @endif
 
 @endsection
 
