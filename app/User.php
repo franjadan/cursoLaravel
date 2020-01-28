84a802daf1a9fa29014def763b3307fb5971e74b
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2020-01-28 17:17:25 +0100

    Poner twitter a null en caso de que un usuario elimine el campo por consola

--- app/User.php
+++ app/User.php
@@ -44,7 +44,7 @@ class User extends Authenticatable
     
             $user->profile()->create([
                 'bio' => $data['bio'],
-                'twitter' => $data['twitter']
+                'twitter' => $data['twitter'] ?? null
             ]);
         });
     }
