4a2f802cc2b5e7dbef87f94a9313e57f763829e4
Francisco Jesus Adan Viedma <francisco.adan@escuelaestech.es>
2020-02-10 21:08:39 +0100

    Filtrar y buscar en función si el usuario tiene un team o no

--- resources/views/users/_filters.blade.php
+++ resources/views/users/_filters.blade.php
@@ -1,5 +1,15 @@
-<form method="get" action="{{ url('usuarios') }}">
-    <div class="row row-filters mt-3">
+<form method="get" action="{{ url('usuarios') }}" class="mt-3">
+    <div class="row rpw-filters">
+        <div class="col-12">
+            @foreach(['' => 'Todos', 'with_team' => 'Con equipo', 'without_team' => 'Sin equipo'] as $value => $text)
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="team" id="team_{{ $value ?: 'all' }}'" value="{{ $value }}" {{ $value === request('team', '') ? 'checked' :  ''}}>
+                    <label class="form-check-label" for="{{ $value ?: 'all' }}">{{ $text }}</label>
+                </div>
+            @endforeach
+        </div>
+    </div>
+    <div class="row row-filters">
         <div class="col-md-6">
             <div class="form-inline form-search">
                 <div class="input-group">
